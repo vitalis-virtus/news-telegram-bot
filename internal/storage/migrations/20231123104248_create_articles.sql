d27e5365b7bc9b51cc98ff181a662e6afb9a88a3
vitalis-virtus <vshayuk@gmail.com>
2023-11-23 10:56:07 +0200

    add migrations

---
+++ internal/storage/migrations/20231123104248_create_articles.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE
+    articles (
+        id SERIAL PRIMARY KEY,
+        source_id INT NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        link VARCHAR(255) NOT NULL,
+        summary TEXT NOT NULL,
+        published_at TIMESTAMP NOT NULL, -- in original resource
+        created_at TIMESTAMP NOT NULL, -- in our DB
+        posted_at TIMESTAMP
+    );
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS articles;
+
+-- +goose StatementEnd
