7a65d6e7ed1fc4a54f6097df29cffec67c5246a0
vitalis-virtus <vshayuk@gmail.com>
2023-11-23 11:23:40 +0200

    add rss source fetching

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/vitalis-virtus/news-telegram-bot
 
 go 1.20
+
+require (
+	github.com/SlyMarbo/rss v1.0.5 // indirect
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
+	github.com/samber/lo v1.38.1 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+)
--- internal/model/model.go
+++ internal/model/model.go
@@ -1,1 +1,30 @@
 package model
+
+import "time"
+
+type Item struct { //for RSS
+	Title      string
+	Categories []string
+	Link       string
+	Date       time.Time
+	Summary    string
+	SourceName string
+}
+
+type Source struct {
+	ID        int
+	Name      string
+	FeedURL   string
+	CreatedAt time.Time
+}
+
+type Article struct { //article in the format in our system
+	ID          int
+	Source      string
+	Title       string
+	Link        string
+	Summary     string
+	PublishedAt time.Time
+	CreatedAt   time.Time
+	PostedAt    time.Time
+}
